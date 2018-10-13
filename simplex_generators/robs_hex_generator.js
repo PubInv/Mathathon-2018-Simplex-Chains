3aed4ffe2ab5c7b42e2fb97389eda6c7b12a3759
Robert L. Read <read.robert@gmail.com>
2018-10-12 21:22:18 -0500

    adding license

--- simplex_generators/robs_hex_generator.js
+++ simplex_generators/robs_hex_generator.js
@@ -1,1 +1,21 @@
-// This is Rob's inital attempt to create a generator that is a simple hexagon.
+// robs_hex_generator.js This is Rob's inital attempt to create a generator that is a simple hexagon.
+// This requires us to standardize on a "type" for generators.
+
+// Copyright (C) 2018  Robert L. Read <read.robert@gmail.com
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
