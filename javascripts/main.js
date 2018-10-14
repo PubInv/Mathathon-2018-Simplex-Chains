6433b4507e518aade84cf38d529530604599b6a6
David Jeschke <david@xtramath.org>
2018-10-13 18:44:04 -0700

    Fix typo in copyright notice.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2018  Robert L. Read <read.robert@gmail.com
+// Copyright (C) 2018  Robert L. Read <read.robert@gmail.com>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
