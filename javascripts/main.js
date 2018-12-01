e83c9c75d6af049ad4d116e117aab7e6ba51d8d5
David Jeschke <david@xtramath.org>
2018-12-01 10:04:44 -0800

    Add GNU public license to 3D playground

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,6 @@
-// Copyright (C) 2018  Robert L. Read <read.robert@gmail.com>
+// Copyright (C) 2018 by
+//   Robert L. Read <read.robert@gmail.com>
+//   David Jeschke <djeschke@gmail.com>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
